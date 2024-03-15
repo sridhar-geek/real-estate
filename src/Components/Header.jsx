a8ddaabf8e3d3e91077d001ca016a21adeca280e
Sridhar <sridharmoturu@gmail.com>
2024-03-15 14:16:36 +0530

    intial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@mui/material"
+
+
+const Header = () => {
+  return (
+<Box>
+    <Box>
+        
+    </Box>
+    <Box>
+
+    </Box>
+    <Box>
+
+    </Box>
+</Box>
+  )
+}
+
+export default Header
